3809ed70169c7043d3e058f6ee1742436b011a15
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-01-28 14:53:12 +0100

    fixed path error

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 source .ve/bin/activate
 
-git pull --no-edit && pip install -r requirements.txt | grep -v "Requirement already satisfied" && ./demo/manage.py migrate && ./site/manage.py collectstatic --noinput
+git pull --no-edit && pip install -r requirements.txt | grep -v "Requirement already satisfied" && ./demo/manage.py migrate && ./demo/manage.py collectstatic --noinput
 echo "touching to reload uwsgi..."
 touch /tmp/pluralsight1-develop.pid
 echo "...finished"
