e5d80f983f46a37cbf5854fe7c1523df23b461e8
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-01-28 14:26:52 +0100

    adjusted file paths to project

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source ../.ve/bin/activate
+source .ve/bin/activate
 
-git pull --no-edit && pip install -r requirements.txt | grep -v "Requirement already satisfied" && ./site/manage.py migrate && ./site/manage.py collectstatic --noinput
+git pull --no-edit && pip install -r requirements.txt | grep -v "Requirement already satisfied" && ./demo/manage.py migrate && ./site/manage.py collectstatic --noinput
 echo "touching to reload uwsgi..."
 touch /tmp/pluralsight1-master.pid
 echo "...finished"
