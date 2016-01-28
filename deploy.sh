81de9a740987a69f902cf84e0de31138de0623c9
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-01-28 14:34:40 +0100

    deploy script: adjusted uwsgi pid file name

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ source .ve/bin/activate
 
 git pull --no-edit && pip install -r requirements.txt | grep -v "Requirement already satisfied" && ./demo/manage.py migrate && ./site/manage.py collectstatic --noinput
 echo "touching to reload uwsgi..."
-touch /tmp/pluralsight1-master.pid
+touch /tmp/pluralsight1-develop.pid
 echo "...finished"
